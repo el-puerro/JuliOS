7d80b898bfc054c9a14cb1b3cdba8c6d3170d67e
Steven Mielke <mielkesteven@icloud.com>
2022-03-27 14:48:39 +0200

    new message for combined commit

---
+++ auto-installation-script/install.sh
@@ -0,0 +1,37 @@
+!#/bin/bash
+#dependencies: archinstall.sh, ascii_art_julios.txt
+# written by Steven Mielke under the GNU general Public License
+
+#initialise german output
+echo ascii_art_julios.txt
+echo "\n\n\n\nInitialising german keyboard layout..."
+loadkeys de-latin1
+echo "done\n"
+
+#check for internet connection
+echo "checking for connection..."
+
+echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    RESULT =$(echo "online")
+    echo "Online\n\n"
+else
+    RESULT=$(echo "offline")
+    echo "Offline\n\n"
+fi
+
+#download dependencies
+wget https://goo.gl/YbxT3F -O archinstall.sh && chmod +x archinstall.sh
+
+
+echo "checking boot mode..."
+#check boot mode (UEFI or legacy BIOS)
+EFIDIR="/sys/firmware/efi/vars"
+if [-d "$EFIDIR" ]; then
+    echo "Booted in UEFI mode\n"
+else
+    echo "booted in BIOS mode\n"
+fi
+
+echo "These are the disks where you can install JuliOS: \n\n"
+lsblk -d -n -oNAME,RO | grep '0$' | awk {'print $1'}
