871ab6382382e49b2689b25137b53290d9e748a1
Steven Mielke <mielkesteven@icloud.com>
2022-03-28 18:17:13 +0200

    switch to main installer from ArchTitus fork

--- auto-installation-script/install.sh
+++ auto-installation-script/install.sh
@@ -1,9 +1,10 @@
 !#/bin/bash
-#dependencies: archinstall.sh, ascii_art_julios.txt
-# written by Steven Mielke under the GNU general Public License
+#dependencies: ascii_art_julios.txt, archtitus fork
+# written by Steven Mielke 
+
 
 #initialise german output
-echo "$(<ascii_art_julios.txt)"
+#echo "$(<ascii_art_julios.txt)"
 echo "\n\n\n\nInitialising german keyboard layout..."
 loadkeys de-latin1
 echo "done\n"
@@ -22,29 +23,34 @@ fi
 
 #download dependencies
 if [ $RESULT = "online" ]; then
-    echo "downloading dependencies\n"
-    curl -O https://goo.gl/YbxT3F && chmod +x archinstall.sh
+    echo "initialise install...\n"
+    #curl -O https://goo.gl/YbxT3F && chmod +x archinstall.sh
+    pacman -Sy git
+    git clone https://github.com/el-puerro/ArchTitus.git
+    cd ArchTitus
+    ./archtitus.sh
 else 
     echo "no internet connection available, exiting...\n"
     exit
 fi
 
 
-echo "checking boot mode..."
+#echo "checking boot mode..."
 #check boot mode (UEFI or legacy BIOS)
-EFIDIR="/sys/firmware/efi/vars"
-if [-d "$EFIDIR" ]; then
-    echo "Booted in UEFI mode\n"
-else
-    echo "booted in BIOS mode\n"
-fi
+#EFIDIR="/sys/firmware/efi/vars"
+#if [-d "$EFIDIR" ]; then
+#    echo "Booted in UEFI mode\n"
+#    echo "Booted in UEFI mode\n"
+#else
+#    echo "booted in BIOS mode\n"
+#fi
 
 #installation target selection
-echo "These are the disks where you can install JuliOS: \n\n"
-lsblk -d -n -oNAME,RO | grep '0$' | awk {'print $1'}
-echo "enter the disk you want to install JuliOS to (usually "sd<x> or something"): "
-read TARGET
-echo "selected installation disk: $TARGET\n"
+#echo "These are the disks where you can install JuliOS: \n\n"
+#lsblk -d -n -oNAME,RO | grep '0$' | awk {'print $1'}
+#echo "enter the disk you want to install JuliOS to (usually "sd<x> or something"): "
+#read TARGET
+#echo "selected installation disk: $TARGET\n"
 
 #running archinstall.sh
-./archinstall.sh --target /dev/$TARGET --host-name JuliOS --user-names juliafiliz --local-time /Europe/Berlin --keyboard-layout de-latin1 --country-with-mirrors Germany --prevent-using-pacstrap --needed-services sshd dhcpcd --install-common-additional-packages --reboot
+#./archinstall.sh --target /dev/$TARGET --host-name JuliOS --user-names juliafiliz --local-time /Europe/Berlin --keyboard-layout de-latin1 --country-with-mirrors Germany --prevent-using-pacstrap --needed-services sshd dhcpcd --install-common-additional-packages --reboot
